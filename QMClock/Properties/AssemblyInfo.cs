707f7ce63b8cd0bc5247237246bd4211c76e7632
DDAkebono <gandvando@hotmail.com>
2021-10-14 23:35:25 -0500

    Initial commit of QMClock

---
+++ QMClock/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using MelonLoader;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("QMClock")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("QMClock")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("39D21149-B232-4C5F-8473-B4E5C89AA474")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: MelonInfo(typeof(QMClock.QMClock), QMClock.BuildInfo.Name, QMClock.BuildInfo.Version, QMClock.BuildInfo.Author, QMClock.BuildInfo.DownloadLink)]
+[assembly: MelonGame("VRChat", "VRChat")]
+[assembly: MelonColor(System.ConsoleColor.Magenta)]
---
+++ QMClock/QMClock.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using MelonLoader;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using VRC.UI.Core;
+using VRC.UI.Elements;
+using Log = MelonLoader.MelonLogger;
+using Object = UnityEngine.Object;
+
+namespace QMClock
+{
+    public static class BuildInfo
+    {
+        public const string Name = "QMClock"; // Name of the Mod.  (MUST BE SET)
+        public const string Author = "DDAkebono#0001"; // Author of the Mod.  (Set as null if none)
+        public const string Company = "BTK-Development"; // Company that made the Mod.  (Set as null if none)
+        public const string Version = "1.0.0"; // Version of the Mod.  (MUST BE SET)
+        public const string DownloadLink = null; // Download Link for the Mod.  (Set as null if none)
+    }
+    
+    public class QMClock : MelonMod
+    {
+        
+        //Prefs
+        private readonly string _prefsCategory = "QMClock";
+        private readonly string _prefs12HourClock = "12HourClock";
+        private readonly string _prefsShowsSeconds = "ShowSeconds";
+
+        private TMP_Text _clock;
+        
+        //Reference GameObjects
+        private GameObject _pingText;
+        private GameObject _panelRoot;
+        
+        private int _scenesLoaded = 0;
+        private string _timeFormat;
+
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            if (_scenesLoaded <= 2)
+            {
+                _scenesLoaded++;
+                if (_scenesLoaded == 2)
+                    UiManagerInit();
+            }
+        }
+
+        public override void OnPreferencesSaved()
+        {
+            _timeFormat = GetTimeFormat();
+        }
+
+        private void UiManagerInit()
+        {
+            Log.Msg("Starting up QMClock!");
+            
+            if (MelonHandler.Mods.Any(x => x.Info.Name.Equals("BTKCompanionLoader", StringComparison.OrdinalIgnoreCase)))
+            {
+                Log.Msg("Hold on a sec! Looks like you've got BTKCompanion installed, this mod is built in and not needed!");
+                Log.Error("QMClock has not started up! (BTKCompanion Running)");
+                return;
+            }
+
+            MelonPreferences.CreateCategory(_prefsCategory, "Quick Menu Clock");
+            MelonPreferences.CreateEntry<bool>(_prefsCategory, _prefs12HourClock, false, "12 Hour Clock Mode");
+            MelonPreferences.CreateEntry<bool>(_prefsCategory, _prefsShowsSeconds, false, "Show Seconds");
+
+            _timeFormat = GetTimeFormat();
+            
+            MelonCoroutines.Start(WaitForQMInit());
+        }
+
+        private string GetTimeFormat()
+        {
+            string format = "HH:mm";
+
+            if (MelonPreferences.GetEntry<bool>(_prefsCategory, _prefs12HourClock).Value)
+                format = "h:mm tt";
+
+            if (MelonPreferences.GetEntry<bool>(_prefsCategory, _prefsShowsSeconds).Value)
+                format = format.Replace("mm", "mm:ss");
+
+            return format;
+        }
+
+        private TMP_Text CreateQMTextElement(string elementName)
+        {
+            if (_panelRoot == null && _pingText == null) return null;
+            
+            GameObject newElement = Object.Instantiate(_pingText, _panelRoot.transform, false);
+
+            newElement.name = elementName;
+
+            TMP_Text newElementText = newElement.GetComponent<TMP_Text>();
+            newElementText.richText = true;
+
+            ResizeRootPanel();
+
+            return newElementText;
+        }
+        
+        private bool SetupDebugInfoPanelAndReferences()
+        {
+            DebugInfoPanel debugPanelComponent = Object.FindObjectOfType<DebugInfoPanel>();
+
+            if (debugPanelComponent == null)
+            {
+                Log.Error("Unable to find the DebugInfoPanel!");
+                return false;
+            }
+
+            _panelRoot = debugPanelComponent.Panel;
+            _pingText = debugPanelComponent.PingText.gameObject;
+            GameObject debugPanelBG = _panelRoot.transform.Find("Background").gameObject;
+
+            if (debugPanelBG == null || _pingText == null)
+            {
+                Log.Error("Unable to find DebugInfoPanel GameObjects");
+                return false;
+            }
+
+            //Copy background image to panel
+            Image background = debugPanelBG.GetComponent<Image>();
+            Image panelBG = _panelRoot.AddComponent<Image>();
+            panelBG.sprite = background.sprite;
+            panelBG.color = new Color(0, 0, 0, .95f);
+            Object.Destroy(debugPanelBG);
+
+            //Setup HorizontalLayoutGroup
+            HorizontalLayoutGroup horizLayout = _panelRoot.AddComponent<HorizontalLayoutGroup>();
+            horizLayout.padding.left = 20;
+            horizLayout.padding.right = 20;
+            horizLayout.spacing = 1.5f;
+
+            return true;
+        }
+        
+        private void CreateClockElement()
+        {
+            _clock = CreateQMTextElement("BTKClockElement");
+            if (_clock == null) return;
+            _clock.text = "00:00";
+        }
+
+        private void ResizeRootPanel()
+        {
+            if (_panelRoot == null) return;
+            
+            RectTransform debugPanelRect = _panelRoot.GetComponent<RectTransform>();
+            Vector2 adjust = debugPanelRect.sizeDelta;
+            adjust.x = 150f*_panelRoot.transform.childCount;
+            debugPanelRect.sizeDelta = adjust;
+        }
+        
+        private DateTime fixedTime()
+        {
+            return TimeZone.CurrentTimeZone.ToLocalTime(DateTime.UtcNow);
+        }
+
+        private IEnumerator TimeUpdate()
+        {
+            while (true)
+            {
+                if (_clock != null)
+                    _clock.text = fixedTime().ToString(_timeFormat);
+
+                yield return new WaitForSeconds(.5f);
+            }
+        }
+        
+        private IEnumerator WaitForQMInit()
+        {
+            while (UIManager.Instance == null) yield return null;
+            while (Object.FindObjectOfType<QuickMenu>() == null) yield return null;
+            
+            SetupPostQMInit();
+        }
+
+        private void SetupPostQMInit()
+        {
+            if (!SetupDebugInfoPanelAndReferences()) return;
+            
+            CreateClockElement();
+            
+            MelonCoroutines.Start(TimeUpdate());
+        }
+    }
+}
